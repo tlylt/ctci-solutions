217b5af95e3504321cdfaca29443642eaa3f4bb8
tlylt <liu_yongliang@hotmail.com>
2020-01-05 19:40:37 +0800

    up to chap1.2

--- ch-01-arrays-and-strings/01-is-unique.py
+++ ch-01-arrays-and-strings/01-is-unique.py
@@ -8,7 +8,50 @@ def contains_no_duplicates(string):
     letters[letter] = True
   return True
 
+# First clarify what is the encoding, is it ASCII or Unicode. Is the string in ASCII or Unicode
+
+#By dictionary
+def is_unique_dict(string):
+  temp = {}
+  for i in string:
+    temp[i]=temp.get(i,0)+1
+  for f in temp.values():
+    if f >1:
+      return False
+  return True
+
+#By array
+###Time: O(n) where n is the length of string, however it could be said to be O(1),
+#since n will be smaller than 128.
+###Space: O(1)
+
+def is_unique_list(string):
+  #assume that we are working with 256 (0-255) ASCII
+  #create an array of boolean values
+  ref = [False]*128
+  #it is impossible to create unique strings longer than the length
+  #of possible base
+  if len(string)>256:
+    return False
+  for i in string:
+    val = ord(i)
+    if ref[val]:
+      return False
+    ref[val]=True
+  return True
+  
+#By bit vector
+#TODO
+
+  
+
+
 if __name__ == "__main__":
-  import sys
-  print(contains_no_duplicates(sys.argv[-1]))
-    
+  t1="hello"
+  t2="world"
+  print('dict')
+  print(is_unique_dict(t1))
+  print(is_unique_dict(t2))
+  print('list')
+  print(is_unique_list(t1))
+  print(is_unique_list(t2))
--- ch-01-arrays-and-strings/02-is-permutation.py
+++ ch-01-arrays-and-strings/02-is-permutation.py
@@ -16,6 +16,47 @@ class Counter(dict):
   def __missing__(self, key):
     return 0
 
+#Ask if string is case sensitive and if whitespace is significant
+
+#By sorting
+def is_permutation_sort(str1,str2):
+  if len(str1)!=len(str2):
+    return False
+  for a,b in zip(sorted(str1),sorted(str2)):
+    if a!=b:
+      return False
+  return True
+
+#By dictionary
+def is_permutation_dict(str1,str2):
+  if len(str1)!=len(str2):
+    return False
+  ref={}
+  for i in str1:
+    ref[i] = ref.get(i,0) + 1
+  for c in str2:
+    if c not in ref:
+      return False
+    ref[c] = ref[c]-1
+  return not sum(ref.values())
+
+#By array
+def is_permutation_list(str1,str2):
+  if len(str1)!=len(str2):
+    return False
+  #assuming ASCII
+  ref=[0]*128
+  for i in str1:
+    ref[ord(i)] +=1
+  for c in str2:
+    if ref[ord(c)]<=0:
+      return False
+    ref[ord(c)] -=1
+  return True
+
 if __name__ == "__main__":
   import sys
   print(is_permutation(sys.argv[-2], sys.argv[-1]))
+  print(is_permutation_dict(sys.argv[-2], sys.argv[-1]))
+  print(is_permutation_sort(sys.argv[-2], sys.argv[-1]))
+  print(is_permutation_list(sys.argv[-2], sys.argv[-1]))
